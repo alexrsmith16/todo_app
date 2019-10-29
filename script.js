833eb7723529e9429bfc04518c0017f4293d5ce4
Alex Smith <alexrmsith16@gmail.com>
2019-10-28 19:24:57 -0600

    All required functionality finished

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let listsArray = [];
 const LISTS_CONTAINER = document.getElementById("lists_container");
 let tx = document.getElementsByTagName('textarea');
 
@@ -6,18 +5,14 @@ if (localStorage.getItem("lists") === null) {
     localStorage.setItem("lists", JSON.stringify([]));
 }
 else {
-    listsArray = JSON.parse(localStorage["lists"]);
-    console.log(listsArray);
-    for (let i of listsArray) {
-        writeList(JSON.parse(localStorage["list_" + i]));
-    }
+    writeEachList();
 }
 
 function watchTextArea() {
     for (let i = 0; i < tx.length; i++) {
         tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
         tx[i].addEventListener("input", OnInput, false);
-        // tx[i].addEventListener("keydown", textAreaKey);
+        tx[i].addEventListener("keyup", textAreaKey);
     }
 }
 watchTextArea();
@@ -28,15 +23,17 @@ function OnInput() {
 }
 
 function createList(name, color = "#688bff") {
+    let listsArray = JSON.parse(localStorage.getItem("lists"));
     let list = {};
-    list.id = listsArray.length === 0 ? 0 : ++listsArray[listsArray.length - 1];
+    list.id = listsArray.length === 0 ? 0 : listsArray[listsArray.length - 1] + 1;
     list.name = name;
     list.color = color;
     list.listItemsArray = [];
     list.listCompletedItems = [];
-    writeList(this);
+    writeList(list);
     localStorage.setItem("list_" + list.id, JSON.stringify(list));
-    localStorage["lists"] = JSON.stringify(listsArray.push(list.id));
+    listsArray.push(list.id);
+    localStorage.setItem("lists", JSON.stringify(listsArray));
 }
 
 const ADD_MODAL = document.getElementById("add_list_modal");
@@ -56,38 +53,94 @@ function addList() {
 document.getElementById("list_name").addEventListener("keydown", event => event.code === "Enter" ? addList() : "");
 
 function closeModal() {
-    console.log("close");
     ADD_MODAL.style.display = "none";
+}
+
+function writeEachList() {
+    LISTS_CONTAINER.innerHTML = "";
+    let listsArray = JSON.parse(localStorage["lists"]);
+    for (let i of listsArray) {
+        writeList(JSON.parse(localStorage["list_" + i]));
+    }
 }
 
 function writeList(list) {
     let newListElement =`
-        <div id="list_${list.id}" class="col-4">
-            <div>
-                <h3>${list.name}</h3>
-                <ul>
-                    <li><textarea id="add_item_list_${list.id}" class="col-12" placeholder="new list item..." rows="1" onkeydown="textAreaKey(event)"></textarea></li>
-                </ul>
-            </div>
-        </div>
-        `;
+        <div id="list_${list.id}" class="col-4"><div>
+            <h2>${list.name}</h2>
+            <ul id="list_${list.id}_items" data-id="${list.id}">`;
+        for (let i in list.listItemsArray) {
+            newListElement += `
+            <li data-id="${i}" class="active-item">
+                <div class="text-holder">${list.listItemsArray[i]}</div>
+                <div class="checkbox-holder">
+                    <div type="checkbox" class="checkbox complete"><label onclick="completeItem(event)">&#10003;</label></div>
+                    <div type="checkbox" class="checkbox delete"><label onclick="deleteItem(event)">&#65794;</label></div>
+                </div>
+            </li>`;
+        }
+        newListElement += `</ul><ul><li><textarea data-id="${list.id}" class="col-12" placeholder="new list item..." rows="1"></textarea></li>`
+        for (let i in list.listCompletedItems) {
+            newListElement += `<li class="completed-item">${list.listCompletedItems[i]}</li>`;
+        }
+        newListElement +=`</ul>
+        <button data-id="${list.id}" onclick="clearItems(event)">Clear Completed Items</button>
+        <button data-id="${list.id}" onclick="deleteList(event)">Delete List</button>
+        </div></div>`;
     LISTS_CONTAINER.insertAdjacentHTML("beforeend", newListElement);
     watchTextArea();
 }
 
-function deleteList(list) {
-    listsArray = listsArray.filter(i => i !== list.id);
-    localStorage["lists"] = listsArray;
-    localStorage.removeItem("list_" + list.id);
-}
-
-function textAreaKey(e) {
-    if (e.which === 13) {
-        console.log(e.target);
-        addItem(e.target)
+function textAreaKey(event) {
+    if (event.keyCode === 13) {
+        addItem(event.target.getAttribute("data-id"), event.target.value);
+        event.target.value = "";
     }
 }
 
-function addItem(element) {
+function addItem(listId, text) {
+    let list = JSON.parse(localStorage.getItem("list_" + listId));
+    list.listItemsArray.push(text);
+    localStorage.setItem("list_" + listId, JSON.stringify(list));
+    writeEachList();
+}
 
+function completeItem(event) {
+    let target = event.target;
+    let itemID = target.parentElement.parentElement.parentElement.getAttribute("data-id");
+    let listID = target.parentElement.parentElement.parentElement.parentElement.getAttribute("data-id");
+    let retrievedList = JSON.parse(localStorage.getItem("list_" + listID));
+    retrievedList.listCompletedItems.push(retrievedList.listItemsArray.splice(itemID, 1));
+    localStorage.setItem("list_" + listID, JSON.stringify(retrievedList));
+    writeEachList();
+}
+
+function deleteItem(event) {
+    let target = event.target;
+    let itemID = target.parentElement.parentElement.parentElement.getAttribute("data-id");
+    let listID = target.parentElement.parentElement.parentElement.parentElement.getAttribute("data-id");
+    let retrievedList = JSON.parse(localStorage.getItem("list_" + listID));
+    retrievedList.listItemsArray.splice(itemID, 1);
+    localStorage.setItem("list_" + listID, JSON.stringify(retrievedList));
+    writeEachList();
+}
+
+function clearItems(event) {
+    let listID = "list_" + event.target.getAttribute("data-id");
+    let list = JSON.parse(localStorage.getItem(listID));
+    list.listCompletedItems = [];
+    console.log(list);
+    localStorage.setItem(listID, JSON.stringify(list));
+    writeEachList();
+}
+
+function deleteList(event) {
+    let listID = parseInt(event.target.getAttribute("data-id"));
+    localStorage.removeItem("list_" + listID);
+    let lists = JSON.parse(localStorage.getItem("lists"));
+    let index = lists.indexOf(listID);
+    console.log(index);
+    lists.splice(index, 1);
+    localStorage.setItem("lists", JSON.stringify(lists));
+    writeEachList();
 }
